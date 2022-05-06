a1d1b8f0b5fe07f834fd123d175501c5ef7ed6d9
Srinandini Marpaka <srinandinim@hotmail.com>
2022-05-06 00:24:26 -0400

    set up

---
+++ src/final.py
@@ -0,0 +1,214 @@
+from hashlib import new
+from prolog_structures import Rule, RuleBody, Term, Function, Variable, Atom, Number
+from typing import List
+from functools import reduce
+
+import sys
+import random
+
+class Not_unifiable(Exception):
+	pass
+
+'''
+Please read prolog_structures.py for data structures
+that represent Prolog terms, rules, and goals.
+'''
+class Interpreter:
+	def __init__(self):
+		pass
+
+	'''
+	Example
+	occurs_check (v, t) where v is of type Variable, t is of type Term.
+	occurs_check (v, t) returns true if the Prolog Variable v occurs in t.
+	Please see the lecture note Control in Prolog to revisit the concept of
+	occurs-check.
+	'''
+	def occurs_check (self, v : Variable, t : Term) -> bool:
+		if isinstance(t, Variable):
+			return v == t
+		elif isinstance(t, Function):
+			for t in t.terms:
+				if self.occurs_check(v, t):
+					return True
+			return False
+		return False
+
+
+	'''
+	Problem 1
+	variables_of_term (t) where t is of type Term.
+	variables_of_clause (c) where c is of type Rule.
+
+	The function should return the Variables contained in a term or a rule
+	using Python set.
+
+	The result must be saved in a Python set. The type of each element (a Prolog Variable)
+	in the set is Variable.
+	'''
+	def variables_of_term (self, t : Term) -> set :
+		variables = set()
+		for term in t.terms:
+			if isinstance(term, Variable):
+				variables.add(term)
+		return variables
+
+	def variables_of_clause (self, c : Rule) -> set :
+		return self.variables_of_term(c.head)
+
+
+	'''
+	Problem 2
+	substitute_in_term (s, t) where s is of type dictionary and t is of type Term
+	substitute_in_clause (s, t) where s is of type dictionary and c is of type Rule,
+
+	The value of type dict should be a Python dictionary whose keys are of type Variable
+	and values are of type Term. It is a map from variables to terms.
+
+	The function should return t_ obtained by applying substitution s to t.
+
+	Please use Python dictionary to represent a subsititution map.
+	'''
+	def substitute_in_term (self, s : dict, t : Term) -> Term:
+		if isinstance(t, Function):
+			new_terms = []
+			for term in t.terms:
+				new_terms.append(self.substitute_in_term(s, term))
+			return Function(t.relation, new_terms)
+		elif isinstance(t, Variable):
+			return s.get(t, t)
+
+		return t
+
+	def substitute_in_clause (self, s : dict, c : Rule) -> Rule:
+		new_head = self.substitute_in_term(s, c.head)
+		new_body = self.substitute_in_term(s, c.body)
+		return Rule(new_head, new_body)
+
+	'''
+	Problem 3
+	unify (t1, t2) where t1 is of type term and t2 is of type Term.
+	The function should return a substitution map of type dict,
+	which is a unifier of the given terms. You may find the pseudocode
+	of unify in the lecture note Control in Prolog useful.
+
+	The function should raise the exception raise Not_unfifiable (),
+	if the given terms are not unifiable.
+
+	Please use Python dictionary to represent a subsititution map.
+	'''
+	def unify_helper(self, t1 : Term, t2 : Term, unifier : dict):
+		# print("made checkpoint 3")
+		# print(str(t1) + "\t" + str(t2))
+		# print ([str(key) + "/" + str(unifier[key]) for key in unifier.keys()])
+
+		X = self.substitute_in_term(unifier, t1)
+		Y = self.substitute_in_term(unifier, t2)
+
+		if isinstance(X, Variable) and not self.occurs_check(X, Y):
+			s = dict({})
+			s[X] = Y
+			for key in unifier.keys():
+				unifier[key] = self.substitute_in_term(s, unifier[key])
+			unifier[X] = Y
+			return unifier
+		elif isinstance(Y, Variable) and not self.occurs_check(Y, X):
+			s = dict({})
+			s[Y] = X
+			for key in unifier.keys():
+				unifier[key] = self.substitute_in_term(s, unifier[key])
+			unifier[Y] = X
+			return unifier
+		elif (type(X) is type(Y)) and (isinstance(X, Atom) or isinstance(X, Number) or isinstance(X, Variable)) and (X == Y):
+			return unifier
+		elif isinstance(X, Function) and isinstance(Y, Function) and len(X.terms) == len(Y.terms):
+			for i in range(0, len(X.terms)):
+				unifier = self.unify_helper(X.terms[i], Y.terms[i], unifier)
+			return unifier
+		else:
+			raise Not_unifiable()
+
+	def unify (self, t1: Term, t2: Term) -> dict:
+		return self.unify_helper(t1, t2, {})
+
+
+	fresh_counter = 0
+	def fresh(self) -> Variable:
+		self.fresh_counter += 1
+		return Variable("_G" + str(self.fresh_counter))
+	def freshen(self, c: Rule) -> Rule:
+		c_vars = self.variables_of_clause(c)
+		theta = {}
+		for c_var in c_vars:
+			theta[c_var] = self.fresh()
+
+		return self.substitute_in_clause(theta, c)
+
+
+	'''
+	Problem 4
+	Following the Abstract interpreter pseudocode in the lecture note Control in Prolog to implement
+	a nondeterministic Prolog interpreter.
+
+	nondet_query (program, goal) where
+		the first argument is a program which is a list of Rules.
+		the second argument is a goal which is a list of Terms.
+
+	The function returns a list of Terms (results), which is an instance of the original goal and is
+	a logical consequence of the program. See the tests cases (in src/main.py) as examples.
+	'''
+	def nondet_query (self, program : List[Rule], pgoal : List[Term]) -> List[Term]:
+		G = pgoal.copy()
+		resolvent = G.copy()
+
+		while len(resolvent) > 0:
+			random_goal_index = random.randint(0, len(resolvent) - 1)
+			A = resolvent[random_goal_index]
+
+			unifiable_clauses = []
+			for clause in program:
+				refreshed_rule = self.freshen(clause)
+				try:
+					self.unify(A, refreshed_rule.head)
+					unifiable_clauses.append(refreshed_rule)
+				except Not_unifiable:
+					continue
+
+			if len(unifiable_clauses) == 0:
+				break
+
+			random_clause_index = random.randint(0, len(unifiable_clauses) - 1)
+			A1 = self.freshen(unifiable_clauses[random_clause_index])
+			unifier = self.unify(A, A1.head)
+
+			resolvent.remove(A)
+			resolvent.extend(A1.body.terms)
+
+			copyG = G.copy()
+			G.clear()
+
+			for goal in copyG:
+				G.append(self.substitute_in_term(unifier, goal))
+			for index in range(len(resolvent)):
+				resolvent[index] = self.substitute_in_term(unifier, resolvent[index])
+
+		if len(resolvent) == 0:
+			return G
+		else:
+			return self.nondet_query(program, pgoal)
+
+
+	'''
+	Challenge Problem
+
+	det_query (program, goal) where
+		the first argument is a program which is a list of Rules.
+		the second argument is a goal which is a list of Terms.
+
+	The function returns a list of term lists (results). Each of these results is
+	an instance of the original goal and is a logical consequence of the program.
+	If the given goal is not a logical consequence of the program, then the result
+	is an empty list. See the test cases (in src/main.py) as examples.
+	'''
+	def det_query (self, program : List[Rule], pgoal : List[Term]) -> List[List[Term]]:
+		return [pgoal]
---
+++ src/temp.py
@@ -0,0 +1,55 @@
+import sys
+import traceback
+
+from prolog_structures import Rule, RuleBody, Term, Function, Variable, Atom, Number
+from final import Interpreter, Not_unifiable
+
+def list2str(l):
+		return ('(' + (',' + ' ').join(
+			list(map(str, l))) + ')')
+
+if __name__ == '__main__':
+
+	interpreter = Interpreter()
+	def ancestor (x, y): return Function ("ancestor", [x, y])
+	def father (x, y): return Function ("father", [x, y])
+	def father_consts (x, y):  return father (Atom (x), Atom (y))
+	f1 = Rule (father_consts ("rickard", "ned"), RuleBody([]))
+	f2 = Rule (father_consts ("ned", "robb"), RuleBody([]))
+	r1 = Rule (ancestor (Variable ("X"), Variable ("Y")), RuleBody([father (Variable ("X"), Variable ("Y"))]))
+	r2 = Rule (ancestor (Variable ("X"), Variable ("Y")), \
+					RuleBody([father (Variable ("X"), Variable ("Z")), ancestor (Variable ("Z"), Variable ("Y"))]))
+	pstark = [f1,f2,r1,r2]
+
+	g = [ancestor (Variable("X"), Atom("robb"))]
+	print (f"Goal: {list2str(g)}")
+	g_ = interpreter.nondet_query (pstark, g)
+	print (f"Solution: {list2str(g_)}")
+	assert (g_ == [ancestor (Atom("ned"), Atom("robb"))] or
+					g_ == [ancestor (Atom("rickard"), Atom("robb"))])
+
+	"""
+	nil = Atom("nil")
+	def cons (h, t): return Function ("cons", [h, t])
+	def append (x, y, z): return Function ("append", [x, y, z])
+	c1 = Rule (append (nil, Variable("Q"), Variable("Q")), RuleBody([]))
+	c2 = Rule (append ((cons (Variable("H"), Variable("P"))), Variable("Q"), (cons (Variable("H"), Variable("R")))), \
+	                RuleBody([append (Variable("P"), Variable("Q"), Variable("R"))]))
+	pappend = [c1, c2]
+
+	print (f"\nProgram: {list2str(pappend)}")
+	g = [append (Variable("X"), Variable("Y"), \
+			(cons (Number("1"), (cons (Number("2"), (cons (Number("3"), nil)))))))]
+	print (f"Goal: {list2str(g)}")
+	g_ = interpreter.nondet_query (pappend, g)
+	print (f"Solution: {list2str(g_)}")
+	print (
+	g_ == [append (nil, (cons (Number("1"), (cons (Number("2"), (cons (Number("3"), nil)))))), \
+							(cons (Number("1"), (cons (Number("2"), (cons (Number("3"), nil)))))))] or
+	g_ == [append ((cons (Number("1"), nil)), (cons (Number("2"), (cons (Number("3"), nil)))), \
+							(cons (Number("1"), (cons (Number("2"), (cons (Number("3"), nil)))))))] or
+	g_ == [append ((cons (Number("1"), (cons (Number("2"), nil)))), (cons (Number("3"), nil)), \
+							(cons (Number("1"), (cons (Number("2"), (cons (Number("3"), nil)))))))] or
+	g_ == [append ((cons (Number("1"), (cons (Number("2"), (cons (Number("3"), nil)))))), nil, \
+							(cons (Number("1"), (cons (Number("2"), (cons (Number("3"), nil)))))))] )
+	"""
